af0c17e31a33060c7abb4d3a2456a58bc0466039
Artiana <hei.rojo.3@gmail.com>
2024-06-17 21:15:33 +0300

    test: add test

---
+++ src/main/java/com/example/marcheur/MarcheurApplication.java
@@ -0,0 +1,11 @@
+package com.example.marcheur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcheurApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MarcheurApplication.class, args);
+    }
+}
--- src/test/java/com/example/marcheur/MarcheurApplicationTests.java
+++ src/test/java/com/example/marcheur/MarcheurApplicationTests.java
@@ -3,11 +3,24 @@ package com.example.marcheur;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(classes = MarcheurApplication.class)
 class MarcheurApplicationTests {
 
     @Test
-    void contextLoads() {
+    void testMarcheurReachesDestination3() {
+
+        Carte carte = new Carte();
+        carte.PlanCarte();
+
+        Lieu lieuInitial = carte.getLieu("HEI");
+        Marcheur marcheur = new Marcheur(lieuInitial);
+
+        marcheur.marcherAleatoirement(carte, "ESTI");
+        assertEquals("ESTI", marcheur.getPositionActuelle().getNom());
     }
+
+    
 
 }
